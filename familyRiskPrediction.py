3e52bbdc2507d82ca7767aba8f007ec25c3c0674
abdulgilani <ah-gilani@hotmail.com>
2025-11-24 20:47:26 -0500

    Add exceptions and value error

--- familyRiskPrediction.py
+++ familyRiskPrediction.py
@@ -1,1 +1,21 @@
-def family_increased_risk(base_risk, first_degree_count, second_degree_count):
+def family_increased_risk(base_risk, first_degree_count, second_degree_count, 
+                          first_degree_risk_factor=2.0, second_degree_risk_factor=1.5):
+    """
+    Calculate the increased risk of alzheimer's disease based on family history."""
+    
+    #validates inputs
+    if base_risk < 0:
+        raise ValueError("Base risk must be greater than or equal to 0.")
+    
+
+    try:
+        first_degree_count = int(first_degree_count)
+        second_degree_count = int(second_degree_count)
+    except Exception:
+        raise ValueError("Family member counts must be integers")
+    
+
+    if first_degree_count < 0 or second_degree_count < 0:
+        raise ValueError("Family member count must be greater than or equal to 0")
+    
+    
